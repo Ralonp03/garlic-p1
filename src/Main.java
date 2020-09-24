138dfb05edc186e16e3c63834768907dedfd15aa
Ricardo <pepe@gmail.com>
2020-09-25 01:26:25 +0200

    Este programa crea una descripcion para un libro

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,41 @@
 
 
 		public static void main(String[] args){
-			System.out.println("Hello world");
-				int x = 0;
+			book sc1 = new book("frido",455);
+			sc1.descripcion("Es un libro de aventuras");
+			sc1.libro();
+			
+			
 			}
 
 		}
+	
+
+	class book{
+		
+	String name;
+	int pages;
+	String info;
+		
+			book(String name, int pages){
+			this.name = name;
+			this.pages = pages;
+				
+			}
+		
+	void descripcion(String info){
+		
+		this.info = info;
+		
+	}
+	void libro(){
+	System.out.println("El libro "+name+" tiene "+pages+" paginas y trata de: \n "+info);	
+	}
+		
+		
+		
+		
+		
+		
+		
+	}
